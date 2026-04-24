0436f08de873e8fe7a1fb8961ecd851fb4e3bb13
Roopansh Sethi <38037190+roopansh123@users.noreply.github.com>
2026-04-24 23:57:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let w = document.querySelector('.w');
+let h = document.querySelector('.h');
+
+window.addEventListener("resize", ()=>{
+	w.textContent = window.innerWidth;
+	h.textContent = window.innerHeight;
+})
